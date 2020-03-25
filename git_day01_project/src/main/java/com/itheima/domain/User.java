eae008f430128cf5852254909ba532aaf7b96f3f
iplaycf <280031296@qq.com>
2020-03-26 00:30:44 +0800

    修改User.java

--- git_day01_project/src/main/java/com/itheima/domain/User.java
+++ git_day01_project/src/main/java/com/itheima/domain/User.java
@@ -3,15 +3,25 @@ package com.itheima.domain;
 public class User {
     private Integer id;
     private String name;
+    private String password;
+
 
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
                 '}';
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
     public Integer getId() {
         return id;
     }
